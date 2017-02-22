5450c0f2dc3fba3489754a47784c12c7aa0b6e74
llwyd <tom@llwyd.io>
2017-02-22 15:12:06 +0000

    Can detect unused squares, minor error in counting that needs to be corrected

--- mineBot.cpp
+++ mineBot.cpp
@@ -66,6 +66,44 @@ BOOL captureFrame(Mat &f,HWND mineHandle,int height, int width){
 	return TRUE;
 }
 
+
+BOOL countSquares(Mat &f,Mat &t, Mat &r, int &count){
+	//Frame,Template,Result,Number of squares
+	Mat greysrc = f.clone();
+	Mat greyTemp = t.clone();
+	cvtColor(f, greysrc, COLOR_BGR2GRAY);
+	cvtColor(t, greyTemp, COLOR_BGR2GRAY);
+
+	matchTemplate(greysrc, greyTemp, r, TM_CCOEFF_NORMED);
+	normalize(r, r, 0, 1, NORM_MINMAX, -1, Mat());
+
+	double minVal;
+	double maxVal;
+	Point minLoc;
+	Point maxLoc;
+	Point matchLoc;
+	Point prevMatchLoc;
+
+	for (int i = 0, j = 0; i < 82; i++){
+		//minesweeper has gradient so only detects two of the squares.
+		minMaxLoc(r, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+		//if (maxVal > 0.87){
+		if ((maxVal > 0.940)){
+			//cout << "[" << j++ << "]=" << maxVal << "(" << maxLoc.x << "," << maxLoc.y << ")\n";
+			matchLoc = maxLoc;
+			//if ((abs(matchLoc.y - prevMatchLoc.y) > 1)){
+				cout << abs(matchLoc.y - prevMatchLoc.y) << ",";
+				count = j++;
+				prevMatchLoc = matchLoc;
+				rectangle(r, matchLoc, Point(matchLoc.x + t.cols, matchLoc.y + t.rows), Scalar(0, 0, 0), CV_FILLED, 8, 0);
+				rectangle(f, matchLoc, Point(matchLoc.x + t.cols, matchLoc.y + t.rows), Scalar(0, 0, 255), 1, 8, 0);
+			//}
+		}
+	}
+	greysrc.release();
+	greyTemp.release();
+	return TRUE;
+}
 int main(void){
 	std::cout << "mineBot\nT.Lloyd\n";
 	Mat frame;
@@ -81,6 +119,7 @@ int main(void){
 	RECT winSize;
 
 	int height, width;
+	int unpressed = 0;
 	GetClientRect(mineHandle, &winSize);
 	height = winSize.bottom;
 	width = winSize.right;
@@ -89,10 +128,28 @@ int main(void){
 	Point pt;
 	pt.x = 0;
 	pt.y = 0;
+
+	Mat temp = imread("Images//square.jpg", CV_LOAD_IMAGE_COLOR);
+	//cvtColor(temp, temp, CV_8UC4,0);
+	Mat result;
+
+	captureFrame(frame, mineHandle, height, width);
+	//imwrite("Images//Master.jpg", frame);
+	//result.create(frame.cols - temp.cols + 1, frame.rows - temp.rows + 1, CV_32FC1);
+
 	until endOfTime{
 		if (captureFrame(frame, mineHandle,height,width)==TRUE){
 			if (!frame.empty()){
+				//matchTemplate(frame, temp, result, TM_CCOEFF_NORMED);
+				Mat result;
+				result.create(frame.cols - temp.cols + 1, frame.rows - temp.rows + 1, CV_32FC1);
+				countSquares(frame, temp, result, unpressed);
+				cout << "Unpressed Squares: " << unpressed << "\n";
+				//result.zeros;
+				imshow("R", result);
+				result.release();
 				imshow("Win", frame);
+				//Sleep(2000);
 			}
 		}
 		if (waitKey(30) >= 0) break;
